1ab93e44a37f4a7337ed36000842ecc1543f211a
Agung Sepruloh <agungsepruloh1996@gmail.com>
2019-09-24 18:17:00 +0700

    Berisi perintah menampilkan value ke layar

---
+++ src/com/pbo/PBO11K10118902Latihan1.java
@@ -0,0 +1,27 @@
+package com.pbo;
+
+/**
+ *
+ * # Author
+ * Nama : Agung Sepruloh
+ * Kelas : PBO11K
+ * NIM : 10118902
+ * Deskripsi Program : Program ini berisi perintah menampilkan value ke layar
+ *
+ */
+
+public class PBO11K10118902Latihan1 {
+
+    public static void main(String[] args) {
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "----------------");
+        System.out.println("NAMA\t\t\t: Agung Sepruloh");
+        System.out.println("ALAMAT\t\t\t: Jalan Kolonel Masturi No.27");
+        System.out.println("TEMPAT LAHIR\t: Cimahi");
+        System.out.println("TANGGAL LAHIR\t: 27 September 1996");
+        System.out.println("----------------");
+        System.out.println("HOBI\t\t\t: Nonton Youtube TED Talks");
+        System.out.println("MAKES\t\t\t: Kulit Ayam");
+        System.out.println("MIKES\t\t\t: Susu dan Madu");
+    }
+
+}
